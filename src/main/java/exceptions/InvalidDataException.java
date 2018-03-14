1d2633e62595ab07674559800d07749fd4298a8c
bara <bara@mediamarktsaturn.com>
2018-03-14 09:35:37 +0200

    mvn

--- src/main/java/exceptions/InvalidDataException.java
+++ src/main/java/exceptions/InvalidDataException.java
@@ -2,4 +2,9 @@ package exceptions;
 
 public class InvalidDataException extends Exception{
 	private static final long serialVersionUID = -69637829093235855L;
+
+
+	public InvalidDataException(String message) {
+		super(message);
+	}
 }
